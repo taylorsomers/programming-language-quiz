1f313e3f46dc1c4ca650ffca23a1b9510d666a4e
Taylor Somers <tsomers@yandex.com>
2020-06-05 17:13:56 -0400

    Add blank options to questions and JavaScript logic that initiates alert requesting answer of all questions if any are left blank

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,13 @@ $(document).ready(function() {
     const question4 = $("select#question4").val();
     const question5 = $("select#question5").val();
 
-    if (question1 === "functions-only") {
+    if (question1 === "null" || question2 === "null" || question3 === "null" || question4 === "null" || question5 === "null") {
+      alert("Please answer all the questions. Seriously, it's not that long of a quiz.");
+    } else if (question3 != "other") {
+      $("#c").hide();
       $("#javascript").hide();
-      $("#no").hide();
       $("#python").hide();
-      $("#c").show();
+      $("#no").show();
     } else {
       $("#c").hide();
       $("#no").hide();
