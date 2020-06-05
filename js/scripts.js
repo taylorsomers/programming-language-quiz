3e70df3e6e2f9b4d236e9844470a6f480ccee3ce
Taylor Somers <tsomers@yandex.com>
2020-06-05 14:47:17 -0400

    Add third question to quiz

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
   $("button").click(function(event) {
     const question1 = $("select#question1").val();
 
-    if (question1 === "yes") {
-      $("#python").hide();
+    if (question1 === "functions-only") {
       $("#javascript").hide();
+      $("#no").hide();
+      $("#python").hide();
       $("#c").show();
     } else {
       $("#c").hide();
+      $("#no").hide();
       $("#python").hide();
       $("#javascript").show();
     }
