739edc6f897ea81cda4dbfb6557e840e0b9a9fa2
Taylor Somers <tsomers@yandex.com>
2020-06-05 17:51:54 -0400

    Add two more questions to quiz

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,17 @@ $(document).ready(function() {
     const question3 = $("select#question3").val();
     const question4 = $("select#question4").val();
     const question5 = $("select#question5").val();
+    const question6 = $("select#question6").val();
+    const question7 = $("select#question7").val();
 
-    if (question1 === "null" || question2 === "null" || question3 === "null" || question4 === "null" || question5 === "null") {
+    if (question1 === "null" || question2 === "null" || question3 === "null" || question4 === "null" || question5 === "null" || question6 === "null" || question7 === "null") {
       alert("Please answer all the questions. Seriously, it's not that long of a quiz.");
     } else if (question3 != "other") {
       $("#c").hide();
       $("#javascript").hide();
       $("#python").hide();
       $("#no").show();
-    } else {
+    } else if (question1 === ){
       $("#c").hide();
       $("#no").hide();
       $("#python").hide();
