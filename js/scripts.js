6e9bba62708e10ca5d6eb3a097a83140f91b32b5
Taylor Somers <tsomers@yandex.com>
2020-06-05 15:09:39 -0400

    Add row/column formatting to quiz questions so they look cleaner

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $("button").click(function(event) {
     const question1 = $("select#question1").val();
+    const question2 = $("select#question2").val();
+    const question3 = $("select#question3").val();
+    const question4 = $("select#question4").val();
+    const question5 = $("select#question5").val();
 
     if (question1 === "functions-only") {
       $("#javascript").hide();
