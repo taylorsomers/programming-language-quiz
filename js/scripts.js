a2a96d377b4d68ee2f4bbfb1214b8cd7c713187b
Taylor Somers <tsomers@yandex.com>
2020-06-05 13:24:28 -0400

    Add logic for display of one result when dummy question 1 is answered yes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-(document).ready(function() {
+$(document).ready(function() {
+  $("button").click(function(event) {
+    const question1 = $("select#question1").val();
+
+    if (question1 === "yes") {
+      $("#c").show();
+    } else {
+      $("#javascript").show;
+    }
+
+  event.preventDefault();
   
+  });
+
 });
