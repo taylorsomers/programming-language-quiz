6ddc862c57b75d6b21b701c601ea3ff54791ec1c
Taylor Somers <tsomers@yandex.com>
2020-06-12 10:14:28 -0400

    Re-size jumbotron image to 100% so it will scale correctly on movile device screens

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   $("button").click(function(event) {
-    const question1 = $("select#question1").val();
-    const question2 = $("select#question2").val();
-    const question3 = $("select#question3").val();
-    const question4 = $("select#question4").val();
-    const question5 = $("select#question5").val();
-    const question6 = $("select#question6").val();
-    const question7 = $("select#question7").val();
+    let question1 = $("select#question1").val();
+    let question2 = $("select#question2").val();
+    let question3 = $("select#question3").val();
+    let question4 = $("select#question4").val();
+    let question5 = $("select#question5").val();
+    let question6 = $("select#question6").val();
+    let question7 = $("select#question7").val();
 
     if (question1 === "null" || question2 === "null" || question3 === "null" || question4 === "null" || question5 === "null" || question6 === "null" || question7 === "null") { 
       $("#c").hide();
